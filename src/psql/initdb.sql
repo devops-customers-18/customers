f91d32a2d00931105934761185ddac2689e60746
weikaipan <weikai.pan1023@gmail.com>
2018-10-11 19:31:11 -0400

    Simplify the sql script

--- src/psql/initdb.sql
+++ src/psql/initdb.sql
@@ -3,7 +3,6 @@ CREATE DATABASE customers ;
 \c customers;
 
 CREATE TABLE IF NOT EXISTS Users (
-    Id INT NOT NULL,
     First_Name VARCHAR(20) NOT NULL,
     Last_Name VARCHAR(20) NOT NULL,
     Username VARCHAR(255) NOT NULL,
@@ -12,8 +11,7 @@ CREATE TABLE IF NOT EXISTS Users (
     Address TEXT,
     Phone_Number VARCHAR(20),
     Title VARCHAR(20),
-    Active BOOLEAN NOT NULL,
-    PRIMARY KEY (Id)
+    Active BOOLEAN NOT NULL
 );
 
 
