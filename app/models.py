d44fef8b7f265740e518784975671bc072d62fb1
weikaipan <weikai.pan1023@gmail.com>
2018-10-16 16:34:55 -0400

    Move flask app to root directory

---
+++ app/models.py
@@ -0,0 +1,148 @@
+# Copyright 2016, 2017 John Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Models for Customer
+
+All of the models are stored in this module
+
+Models
+------
+Customer - A Customer used in the Customer Store
+
+"""
+import threading
+
+
+class DataValidationError(Exception):
+    """ Used for an data validation errors when deserializing """
+    pass
+
+
+class Customer(object):
+    """
+    Class that represents a Customer
+
+    This version uses an in-memory collection of customers for testing
+    """
+    lock = threading.Lock()
+    data = []
+    index = 0
+
+    def __init__(self, cust_id=0, first_name='', last_name='',
+                 address='', email='', username='', password='',
+                 phone_number='', active=True):
+        """ Initialize a Customer. """
+        self.id = cust_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.email = email
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+        self.active = active
+        self.customer_dict = {"id": cust_id, "first_name": first_name,
+                              "last_name": last_name, "address": address,
+                              "email": email,
+                              "username": username,
+                              "passward": password,
+                              "phone_number": phone_number,
+                              "active": active}
+
+    def save(self):
+        """
+        Saves a Customer to the data store
+        """
+        if self.id == 0:
+            self.id = self.__next_index()
+            Customer.data.append(self)
+        else:
+            for i in range(len(Customer.data)):
+                if Customer.data[i].id == self.id:
+                    Customer.data[i] = self
+                    break
+
+    def delete(self):
+        """ Removes a Customer from the data store """
+        Customer.data.remove(self)
+
+    def serialize(self):
+        """ Serializes a Customer into a dictionary """
+        return {"id": self.id, "first_name": self.first_name,
+                "last_name": self.last_name, "address": self.address,
+                "email": self.email, "username": self.username,
+                "password": self.password, "phone_number": self.phone_number,
+                "active": self.active}
+
+    def deserialize(self, data):
+        """
+        Deserializes a Customer from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the Customer data
+        """
+        if not isinstance(data, dict):
+            raise DataValidationError('Invalid customer: body of request contained bad or no data')
+        try:
+            self.first_name = data["first_name"]
+            self.last_name = data["last_name"]
+            self.address = data["address"]
+            self.email = data["email"]
+            self.username = data["username"]
+            self.password = data["password"]
+            self.phone_number = data["phone_number"]
+            self.active = data["active"]
+        except KeyError as err:
+            raise DataValidationError('Invalid customer: missing ' + err.args[0])
+        return
+
+    @classmethod
+    def __next_index(cls):
+        """ Generates the next index in a continual sequence """
+        with cls.lock:
+            cls.index += 1
+        return cls.index
+
+    @classmethod
+    def all(cls):
+        """ Returns all of the Customers in the database """
+        return [customer for customer in cls.data]
+
+    @classmethod
+    def remove_all(cls):
+        """ Removes all of the Customers from the database """
+        del cls.data[:]
+        cls.index = 0
+        return cls.data
+
+    @classmethod
+    def find(cls, customer_id):
+        """ Finds a Customer by it's ID """
+        if not cls.data:
+            return None
+        customers = [customer for customer in cls.data if customer.id == customer_id]
+        if customers:
+            return customers[0]
+        return None
+
+    @classmethod
+    def find_by_query(cls, key, value):
+        """ Returns the list of the Customers in a data list which
+        satisfied the query
+
+        Args:
+            key (string): the attributes name
+            value: attributes values
+        """
+        return [customer for customer in cls.data if customer.__dict__.get(key) == value]
---
+++ tests/test_server.py
@@ -0,0 +1,229 @@
+# Copyright 2016, 2017 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test cases for the Pet Service
+
+Test cases can be run with:
+  nosetests
+  coverage report -m
+"""
+
+import logging
+import unittest
+import json
+from flask_api import status    # HTTP Status Codes
+from app.models import DataValidationError
+import app.service as service
+
+######################################################################
+#  T E S T   C A S E S
+######################################################################
+class TestPetServer(unittest.TestCase):
+    """ Pet Server Tests """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Run once before all tests """
+        service.app.debug = False
+        service.initialize_logging(logging.ERROR)
+
+    def setUp(self):
+        """ Runs before each test """
+        service.Customer.remove_all()
+        service.Customer(0, 'fido', 'dog', 'nj', 'a@b.com',
+                        'kerker', 'aa', '932').save()
+        service.Customer(0, 'afido', 'cat', 'ny', 'c@b.com',
+                        'Ker', 'ww', '9321').save()
+        self.app = service.app.test_client()
+
+    def tearDown(self):
+        """ Runs after each test """
+        service.Customer.remove_all()
+
+    def test_index(self):
+        """ Test the Home Page """
+        resp = self.app.get('/')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        self.assertEqual(data['name'], 'Customer Demo REST API Service')
+
+    def test_find_customer(self):
+        """ Find one customer """
+        resp = self.app.get('/customers/2')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        self.assertEqual(data['username'], 'Ker')
+        self.assertEqual(data['first_name'], 'afido')
+        self.assertEqual(data['last_name'], 'cat')
+
+    def test_get_customer_not_found(self):
+        """ Get a Pet thats not found """
+        resp = self.app.get('/customers/0')
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_customer(self):
+        """ Create a customers """
+        # save the current number of pets for later comparrison
+        customers_count = self.get_customers_count()
+        # add a new pet
+        new_customer = {"username": "foo111", "password": "bar",
+                        "first_name":"value1", "last_name":"value2", "id": 0,
+                        "address": "Jersey", "phone_number": "773",
+                        "active": True, "email": "3333"}
+        data = json.dumps(new_customer)
+        resp = self.app.post('/customers', data=data,
+                             content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # Make sure location header is set
+        location = resp.headers.get('Location', None)
+        self.assertIsNotNone(location)
+        # Check the data is correct
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['username'], 'foo111')
+        # check that count has gone up and includes sammy
+        resp = self.app.get('/customers')
+        data = json.loads(resp.data)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(data), customers_count + 1)
+        self.assertIn(new_json, data)
+
+    def test_spoof_customer_id(self):
+        """ Create a customer passing in an id """
+        # add a new pet
+        new_customer = {"username": "foo111", "password": "bar",
+                        "first_name":"value1", "last_name":"value2", "id": 999,
+                        "address": "Jersey", "phone_number": "773",
+                        "active": True, "email": "3333"}
+        data = json.dumps(new_customer)
+        resp = self.app.post('/customers', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # Make sure location header is set
+        location = resp.headers.get('Location', None)
+        self.assertIsNotNone(location)
+        # Check the data is correct
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['username'], 'foo111')
+        self.assertNotEqual(new_json['id'], 999)
+
+    def test_update_customer(self):
+        """ Update a customer """
+        new_customer = {"username": "foo111", "password": "bar",
+                        "first_name":"value1", "last_name":"value2", "id": 0,
+                        "address": "Jersey", "phone_number": "773",
+                        "active": True, "email": "3333"}
+        data = json.dumps(new_customer)
+        resp = self.app.put('/customers/2', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        resp = self.app.get('/customers/2', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['username'], 'foo111')
+
+    def test_disable_customer(self):
+        """ Disable a customer """
+        resp = self.app.put('/customers/1/disable', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        resp = self.app.get('/customers/1', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['active'], 'False')
+
+    def test_update_customer_with_no_name(self):
+        """ Update a customer with no username """
+        new_customer = {"password": "bar", "first_name":"value1",
+                        "last_name":"value2", "address": "Jersey",
+                        "phone_number": "773", "active": True,
+                        "email": "3333"}
+        data = json.dumps(new_customer)
+        resp = self.app.put('/customers/2', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_customer_not_found(self):
+        """ Update a Customer that can't be found """
+        new_man = {"username": "noguy", "password": "bar",
+                   "first_name":"value1", "last_name":"value2", "id": 0,
+                   "address": "Jersey", "phone_number": "773",
+                   "active": True, "email": "3333"}
+        data = json.dumps(new_man)
+        resp = self.app.put('/customers/0', data=data, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_customer(self):
+        """ Delete a customer that exists """
+        # save the current number of pets for later comparrison
+        customer_count = self.get_customers_count()
+        # delete a pet
+        resp = self.app.delete('/customers/2', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
+        new_count = self.get_customers_count()
+        self.assertEqual(new_count, customer_count - 1)
+
+    def test_create_customer_with_no_name(self):
+        """ Create a customer with the name missing """
+        new_customer = {"password": "bar",
+                        "first_name":"value1", "last_name":"value2", "id": 0,
+                        "address": "Jersey", "phone_number": "773",
+                        "active": True, "email": "3333"}
+        data = json.dumps(new_customer)
+        resp = self.app.post('/customers', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_nonexisting_customer(self):
+        """ Get a customer that doesn't exist """
+        resp = self.app.get('/customers/5')
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_customer_list_without_queries(self):
+        """ Get a list of Customer """
+        resp = self.app.get('/customers')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        self.assertEqual(len(data), 2)
+
+    def test_get_customer_list_with_queries(self):
+        """ Get Customers with queries """
+        resp = self.app.get('/customers', query_string='username=kerker')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
+        self.assertTrue('fido' in resp.data)
+        self.assertFalse('afido' in resp.data)
+        data = json.loads(resp.data)
+        query_item = data[0]
+        self.assertEqual(query_item['username'], 'kerker')
+
+    def test_method_not_allowed(self):
+        """ Call a Method thats not Allowed """
+        resp = self.app.post('/customers/0')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+######################################################################
+# Utility functions
+######################################################################
+
+    def get_customers_count(self):
+        """ save the current number of customers """
+        resp = self.app.get('/customers')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        return len(data)
+
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == '__main__':
+    unittest.main()
