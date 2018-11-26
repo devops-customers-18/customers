2692958b909407f962acffac65299a7b7fa1bf44
weikaipan <weikai.pan1023@gmail.com>
2018-11-26 17:33:48 -0500

    All server coverage = 100%

--- tests/test_server.py
+++ tests/test_server.py
@@ -153,6 +153,38 @@ class TestCustomerServer(unittest.TestCase):
         resp = self.app.post('/customers', data=data)
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_customer_form_content_type(self):
+        """ Create a Customer with form Content-Type """
+        customers_count = self.get_customers_count()
+        new_customer = {"username": "foo111", "password": "bar",
+                        "first_name": "value1", "last_name": "value2",
+                        "address": "Jersey", "phone_number": "773",
+                        "active": True, "email": "3333"}
+
+        resp = self.app.post('/customers', data=new_customer, content_type='application/x-www-form-urlencoded')
+        self.assertEqual(resp.status_code, HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = resp.headers.get('Location', None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['username'], 'foo111')
+        self.assertEqual(new_json['first_name'], 'value1')
+        self.assertEqual(new_json['last_name'], 'value2')
+        self.assertEqual(new_json['address'], 'Jersey')
+        self.assertEqual(new_json['email'], '3333')
+        self.assertEqual(new_json['password'], 'bar')
+        self.assertEqual(new_json['phone_number'], '773')
+
+        # check that count has gone up and includes sammy
+        resp = self.app.get('/customers')
+        data = json.loads(resp.data)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(data), customers_count + 1)
+        self.assertIn(new_json, data)
+
     def test_create_customer_with_no_name(self):
         """ Create a customer with the name missing """
         new_customer = {"password": "bar",
