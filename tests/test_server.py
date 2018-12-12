336839107e6262bb74f7ffd74499f44a2f94ab66
Eric Lee <imdfmy@gmail.com>
2018-12-12 00:04:31 -0500

    import os in tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -20,6 +20,7 @@ Test cases can be run with:
   coverage report -m
 """
 
+import os
 import unittest
 import logging
 import json
@@ -60,7 +61,7 @@ class TestCustomerServer(unittest.TestCase):
                  email='a@b.com', username='kerker',
                  password='aa', phone_number='932',
                  active=True).save()
-        
+
         time.sleep(0.5)
 
         Customer(first_name='afido',
